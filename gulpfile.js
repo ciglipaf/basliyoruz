d361f9a1d986c0da9e174f71d4e142732987e8c0
cmlonder <cemalonder55@gmail.com>
2016-06-13 19:31:36 +0300

    sonsuz dongu yapmışız, düzeltildi

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var exec = require('child_process').exec;
 
 gulp.task("ciglipaf", function(cp) {
-  exec("npm test", function (err, stdout, stderr) {
+  exec("ls -la", function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cp(err);
